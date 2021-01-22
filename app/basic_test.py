fcc9a5413320a24b56b23c6ded17c8fc2cdf908e
crouchr <richard.crouch@vonage.com>
2021-01-22 12:05:22 +0000

    Updated before rebuild

--- app/basic_test.py
+++ app/basic_test.py
@@ -5,11 +5,13 @@ import cloudcoverage
 def main():
 
     image = '../test_images/image397.jpg'
+    image = '../ermin_cloud_images/metminiwx_1.png'
+
     coverageimage, coverage_percentage = cloudcoverage.get_coverage(image, is_daytime=True)
 
     # display the processed version of the image
     coverageimage.show()
-    print 'coverage percent = ' + coverage_percentage.__str__()
+    print('coverage percent = ' + coverage_percentage.__str__())
 
 
 # A basic test program to show usage
---
+++ app/definitions.py
@@ -0,0 +1,2 @@
+coverage_service_listen_port = 9502
+coverage_service_endpoint_base = 'http://0.0.0.0:' + coverage_service_listen_port.__str__()
